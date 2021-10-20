6bed9683c8347fc9bd9260bbb706ca6f6dab1d8d
OGGYKUTTAN1 <85207400+irash1234567@users.noreply.github.com>
2021-10-20 20:11:29 +0530

    Update locals.py

--- resources/startup/locals.py
+++ resources/startup/locals.py
@@ -2,9 +2,9 @@
 # Ultroid - UserBot
 # Copyright (C) 2021 TeamUltroid
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/irash1234567/Ultroid >
 # Please read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://github.com/irash1234567/Ultroid/blob/main/LICENSE/>.
 
 # Standalone file for facilitating local deploys.
 
